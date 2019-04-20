104867928b87582112b87764abc4e8decf496713
wx_zhaoqf <fairy_xingyun@hotmail.com>
2019-04-20 10:13:06 +0800

    added Spring Boot web 模块集成

---
+++ spring-boot-with-web-sample/src/main/java/com/xingyun/springbootwithwebsample/SpringBootWithWebSampleApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithwebsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithWebSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithWebSampleApplication.class, args);
+    }
+
+}
