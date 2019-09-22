1027a9d9a5a251611375d13e10ee8ae46c857f1e
fairy <fairy_xingyun@hotmail.com>
2019-09-22 09:01:52 +0800

    added sample for spring boot with i shop sample

---
+++ spring-boot-with-i-shop-sample/src/test/java/com/xingyun/springbootwithishopsample/SpringBootWithIShopSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xingyun.springbootwithishopsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWithIShopSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
