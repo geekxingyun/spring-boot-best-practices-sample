e060770cd5c2a8091a74d5846cb92bfa60a91c0d
qing-feng.zhao <fairy_xingyun@hotmail.com>
2019-11-13 08:44:18 +0800

    update spring boot version to 2.2.1.RELEASE

--- spring-boot-with-customize-banner-sample/src/main/java/com/xingyun/controller/HomeController.java
+++ spring-boot-with-customize-banner-sample/src/main/java/com/xingyun/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.xingyun.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 默认请求页面控制器
+ * @author qing-feng.zhao
+ */
 @RestController
 public class HomeController {
 
