d734410aafdc51d41e3a7091f8a5224b4a6eaa6f
qing-feng.zhao <fairy_xingyun@hotmail.com>
2019-11-14 18:30:15 +0800

    init Spring Boot With Java Mail Sender Sample

---
+++ spring-boot-with-java-mail-sender-sample/src/main/java/com/xingyun/springbootwithjavamailsendersample/SpringBootWithJavaMailSenderSampleApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithjavamailsendersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJavaMailSenderSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithJavaMailSenderSampleApplication.class, args);
+    }
+
+}
