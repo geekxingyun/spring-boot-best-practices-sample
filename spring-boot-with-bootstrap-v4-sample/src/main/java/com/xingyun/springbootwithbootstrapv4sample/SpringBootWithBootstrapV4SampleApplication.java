21fce840e829f1527fd08d8bedbb4b8939ae6cb8
qing-feng.zhao <fairy_xingyun@hotmail.com>
2019-12-27 12:39:00 +0800

    init bootstrap v4 for spring boot project

---
+++ spring-boot-with-bootstrap-v4-sample/src/main/java/com/xingyun/springbootwithbootstrapv4sample/SpringBootWithBootstrapV4SampleApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithbootstrapv4sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithBootstrapV4SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithBootstrapV4SampleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-with-bootstrap-v4-sample/src/test/java/com/xingyun/springbootwithbootstrapv4sample/SpringBootWithBootstrapV4SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithbootstrapv4sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithBootstrapV4SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
