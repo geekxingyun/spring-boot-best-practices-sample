72b25c89ca903a2842ae7b7cb9c99f13e3795dfc
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-03-19 16:43:38 +0800

    init project

---
+++ spring-boot-with-docker-sample/src/main/java/com/xingyun/springbootwithdockersample/SpringBootWithDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithdockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithDockerSampleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-with-docker-sample/src/test/java/com/xingyun/springbootwithdockersample/SpringBootWithDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithdockersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
