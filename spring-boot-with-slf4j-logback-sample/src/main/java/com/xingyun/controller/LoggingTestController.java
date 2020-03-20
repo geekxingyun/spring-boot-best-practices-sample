21f4c478b7ec6c6a270cba6ef67b7f494ccb2aef
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-03-20 15:19:05 +0800

    update spring boot version to 2.2.5.RELEASE

--- spring-boot-with-slf4j-logback-sample/src/main/java/com/xingyun/controller/LoggingTestController.java
+++ spring-boot-with-slf4j-logback-sample/src/main/java/com/xingyun/controller/LoggingTestController.java
@@ -1,9 +1,12 @@
 package com.xingyun.controller;
 
+import com.xingyun.util.LogbackThreadLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 /**
  * @author xingyun
@@ -11,15 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingTestController {
 
-	private Logger logger= LoggerFactory.getLogger(LoggingTestController.class);
+	private static Logger logger= LoggerFactory.getLogger(LoggingTestController.class);
 	
 	@GetMapping(value="/log.do")
 	public String loggingTestMethod() {
-		logger.trace("this is debug message");
 		logger.debug("this is debug message");
 		logger.info("this is info message");
 		logger.warn("this is warn message");
 		logger.error("this is debug message");
+		return "logback test success,please check logs";
+	}
+
+	@GetMapping(value="/log-thread.do")
+	public String loggingThreadTestMethod() {
+
+		Logger logger=LogbackThreadLogger.getLogger("test",LoggingTestController.class.getSimpleName());
+		logger.debug("this is thread debug message");
+		logger.info("this is thread info message");
+		logger.warn("this is thread warn message");
+		logger.error("this is thread error message");
 		return "logback test success,please check logs";
 	}
 }
--- spring-boot-with-slf4j-logback-sample/src/main/java/com/xingyun/util/LogbackThreadLogger.java
+++ spring-boot-with-slf4j-logback-sample/src/main/java/com/xingyun/util/LogbackThreadLogger.java
@@ -24,53 +24,53 @@ public class LogbackThreadLogger extends RollingFileAppender{
     /**
      * App Name
      */
-    public final static String APP_NAME="myApp";
+    public final static String APP_NAME="spring-boot-with-logback-sample";
 
     /**
      * 通用配置
-     * 日志存放路径
+     * 日志存放路径 /opt/logs/spring-boot-with-logback-sample/customize-thread-log/
      */
-    public final static String LOG_FILE_BASE_PATH="/opt/applog/"+APP_NAME+"/log/";
+    public final static String LOG_FILE_BASE_PATH=File.separator+"opt"+File.separator+"logs"+File.separator+APP_NAME+File.separator+"customize-thread-log"+File.separator;
     /**
      * 配置日志输出格式
      */
-    public static final String fileLogLayout="%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level --- %logger{36} --- %c:%L:%n%m%n";
+    public static final String FILE_LOG_LAYOUT="%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level --- %logger{36} --- %c:%L:%n%m%n";
     /**
      * 配置日志编码
      */
-    public static final String charSetName="UTF-8";
+    public static final String CHAR_SET_NAME="UTF-8";
     /**
      * 是否自动追加
      */
-    public static final Boolean append=true;
+    public static final Boolean APPEND=true;
     /**
      * 是否立即刷新
      */
-    public static final Boolean immediateFlush=true;
+    public static final Boolean IMMEDIATE_FLUSH=true;
     /**
      * 单个文件最大值
      */
-    public static final String maxFileSize="100MB";
+    public static final String MAX_FILE_SIZE="100MB";
     /**
      * 单个文件最大值
      */
-    public static final  FileSize maxFileSizeValue=FileSize.valueOf(maxFileSize);
+    public static final  FileSize MAX_FILE_SIZE_VALUE=FileSize.valueOf(MAX_FILE_SIZE);
     /**
      * 保存30天
      */
-    public static final Integer maxHistory=30;
+    public static final Integer MAX_HISTORY=30;
     /**
      * 日志保存最大容量
      */
-    public static final String totalSizeCap="20GB";
+    public static final String TOTAL_SIZE_CAP="20GB";
     /**
      * 日志保存最大容量
      */
-    public static final  FileSize totalSizeCapValue=FileSize.valueOf(totalSizeCap);
+    public static final  FileSize TOTAL_SIZE_CAP_VALUE=FileSize.valueOf(TOTAL_SIZE_CAP);
     /**
      * 启动清理
      */
-    public static final Boolean cleanHistoryOnStart=false;
+    public static final Boolean CLEAN_HISTORY_ON_START=false;
 
     /**
      * 打印线程日志
@@ -80,6 +80,9 @@ public class LogbackThreadLogger extends RollingFileAppender{
      */
     @SuppressWarnings("unused")
     public static Logger getLogger(String folderName,String className){
+
+        //初始化文件夹 如果文件夹不存在则创建
+        createFolder(LOG_FILE_BASE_PATH);
 
         //创建日志实例对象
         Logger logger = (Logger) LoggerFactory.getLogger(className);
@@ -91,14 +94,16 @@ public class LogbackThreadLogger extends RollingFileAppender{
         //配置上下文
         rollingFileAppender.setContext(loggerContext);
         //配置文本追加 如果设置为false则覆盖
-        rollingFileAppender.setAppend(append);
+        rollingFileAppender.setAppend(APPEND);
         //配置立即追加
-        rollingFileAppender.setImmediateFlush(immediateFlush);
-        //配置日志路径和名称 opt/applog/log/spring/log/test/myThread.log
+        rollingFileAppender.setImmediateFlush(IMMEDIATE_FLUSH);
+
+        //配置日志路径和名称 /opt/logs/spring-boot-with-logback-sample/customize-thread-log/myThread.log
         rollingFileAppender.setFile(LOG_FILE_BASE_PATH+folderName+ File.separator+className+".log");
         //配置输出格式和输出字符集
         rollingFileAppender.setEncoder(configPatternLayoutEncoder(loggerContext));
-        //配置滚动策略 opt/applog/log/spring/log/2019-08-20/test/myThread.log
+
+        //配置滚动策略 /opt/logs/spring-boot-with-logback-sample/customize-thread-log/2019-08-20/myThread.log
         rollingFileAppender.setRollingPolicy(configSizeAndTimeBasedRollingPolicy(loggerContext,rollingFileAppender,folderName,className));
         //启动文件追加器
         rollingFileAppender.start();
@@ -109,6 +114,16 @@ public class LogbackThreadLogger extends RollingFileAppender{
         return logger;
     }
 
+    /**
+     * 如果文件不存在则创建它
+     * @param fileFolderName
+     */
+    private static void createFolder(String fileFolderName){
+        File file=new File(fileFolderName);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+    }
 
     /**
      * 日志输出格式和字符集
@@ -121,9 +136,9 @@ public class LogbackThreadLogger extends RollingFileAppender{
         //配置日志上下文
         patternLayoutEncoder.setContext(loggerContext);
         //配置日志输出格式 layout
-        patternLayoutEncoder.setPattern(fileLogLayout);
+        patternLayoutEncoder.setPattern(FILE_LOG_LAYOUT);
         //配置日志输出字符集
-        patternLayoutEncoder.setCharset(Charset.forName(charSetName));
+        patternLayoutEncoder.setCharset(Charset.forName(CHAR_SET_NAME));
         //启动encoder
         patternLayoutEncoder.start();
 
@@ -145,16 +160,16 @@ public class LogbackThreadLogger extends RollingFileAppender{
         sizeAndTimeBasedRollingPolicy.setContext(loggerContext);
         //配置日志追加器
         sizeAndTimeBasedRollingPolicy.setParent(rollingFileAppender);
-        //配置日志滚动策略规则 /opt/applog/myApp/log/2019-08-20/test/myThread.log
+        //配置日志滚动策略规则 /opt/logs/spring-boot-with-logback-sample/2019-08-20/myThread.log
         sizeAndTimeBasedRollingPolicy.setFileNamePattern(LOG_FILE_BASE_PATH+"%d{yyyy-MM-dd,aux}"+File.separator+folderName+File.separator+className+"-%d{yyyy-MM-dd,UTC}.%i.log");
         //设置单个文件大小
-        sizeAndTimeBasedRollingPolicy.setMaxFileSize(maxFileSizeValue);
+        sizeAndTimeBasedRollingPolicy.setMaxFileSize(MAX_FILE_SIZE_VALUE);
         //保存多少天
-        sizeAndTimeBasedRollingPolicy.setMaxHistory(maxHistory);
+        sizeAndTimeBasedRollingPolicy.setMaxHistory(MAX_HISTORY);
         //磁盘最大日志大小
-        sizeAndTimeBasedRollingPolicy.setTotalSizeCap(totalSizeCapValue);
+        sizeAndTimeBasedRollingPolicy.setTotalSizeCap(TOTAL_SIZE_CAP_VALUE);
         //启动时候清理日志
-        sizeAndTimeBasedRollingPolicy.setCleanHistoryOnStart(cleanHistoryOnStart);
+        sizeAndTimeBasedRollingPolicy.setCleanHistoryOnStart(CLEAN_HISTORY_ON_START);
         //开启滚动策略
         sizeAndTimeBasedRollingPolicy.start();
         return sizeAndTimeBasedRollingPolicy;
