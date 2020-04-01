e30207efa4e030a0869acab9c3396922debc22cd
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-04-01 15:16:17 +0800

    init project

---
+++ spring-boot-with-redis-sample/src/test/java/com/xingyun/springbootwithredissample/SpringBootWithRedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithredissample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithRedisSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
