fedb5126ce3be4e086d2ab03e127769fbf0eb929
qing-feng.zhao <fairy_xingyun@hotmail.com>
2021-04-29 10:47:01 +0800

    init spring boot with rabbitmq sample

---
+++ spring-boot-with-rabbitmq-sample/src/main/java/com/xingyun/springbootwithrabbitmqsample/SpringBootWithRabbitmqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.springbootwithrabbitmqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithRabbitmqSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithRabbitmqSampleApplication.class, args);
+    }
+
+}
